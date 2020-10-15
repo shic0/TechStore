0f3fd8a0fbdbea25b229a01e7a630a08005a48e5
ShahinSayadi <Shahin_sayadi@hotmail.com>
2020-10-15 22:33:23 +0200

    Lägger upp det jag hunnit med hitills

--- main.js
+++ main.js
@@ -20,12 +20,52 @@ function initSite() {
 
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
-    // Check your console to see that the products are stored in the listOfProducts varible.
-    console.log(listOfProducts);
-
-    // Add your code here, remember to brake your code in to smaller function blocks
-    // to reduce complexity and increase readability. Each function should have
-    // an explainetory comment like the one for this function, see row 22.
+// Check your console to see that the products are stored in the listOfProducts varible.
     
-    // TODO: Remove the console.log and these comments when you've read them.
+    console.log(listOfProducts);
+    let mainContent = document.getElementById("mainContent")
+     // Kallar på parent elementet till mainContainer
+    
+    let mainContainer = document.createElement("div")
+    mainContainer.style.width = "300px"
+    mainContainer.style.height = "300px"
+    mainContainer.style.display = "flex"
+    mainContainer.style.flexDirection = "column"
+     // skapat mainContainer samt lite styling
+     
+    let titleContainer = document.createElement("div")
+    titleContainer.style.height ="20px"
+    titleContainer.style.innerhtml = title
+
+     // Titeln på telefonen
+
+    let descriptionContainer = document.createElement("div")
+     // Description på telefonen
+     
+    let imgContainer = document.createElement("img")
+    imgcointainer.style.height= "80%"
+    imgcointainer.style.width= "80%"
+     // bilden
+    
+    let priceContainer = document.createElement("div")
+     // Priset på telefonen
+    
+    let buyButton = document.createElement("button")
+     // skicka till varukorg
+    
+    
+    mainContainer.appendChild(descriptionContainer)
+    mainContainer.appendChild(titleContainer)
+    mainContainer.appendChild(imgContainer)
+    mainContainer.appendChild(priceContainer)
+    mainContent.appendChild(mainContainer)
+    mainContainer.appendChild(buyButton)
+     
+    
+    
+    
+     // Add your code here, remember to brake your code in to smaller function blocks
+     // to reduce complexity and increase readability. Each function should have
+     // an explainetory comment like the one for this function, see row 22.
+     // TODO: Remove the console.log and these comments when you've read them.
 }
