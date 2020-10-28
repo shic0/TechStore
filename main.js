b4c0ad57a6caa715860317060114ca533df9e39c
ShahinSayadi <Shahin_sayadi@hotmail.com>
2020-10-28 09:21:44 +0100

    Stylat startsidan samt skapat divar

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var listOfProducts;
-
 /** Get products from the json file and store it in a gobal variable */
 function loadProducts() {
     fetch("./products.json")
@@ -13,53 +12,107 @@ function loadProducts() {
 }
 
 
+
+
 function initSite() {
     loadProducts();
+    
     // This would also be a good place to initialize other parts of the UI
 }
+
 
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
 // Check your console to see that the products are stored in the listOfProducts varible.
     
-    console.log(listOfProducts);
+    for (let i = 0; i < listOfProducts.length; i++) {
+        let productList = listOfProducts[i]
+        console.log(productList)
+        
+        
+        
+        
+
+    }
+    
+    
+
+    
     let mainContent = document.getElementById("mainContent")
+    mainContent.style.display = "flex"
+    mainContent.style.justifyContent = "space-around"
+
+   
+
      // Kallar på parent elementet till mainContainer
     
     let mainContainer = document.createElement("div")
-    mainContainer.style.width = "300px"
-    mainContainer.style.height = "300px"
+  
     mainContainer.style.display = "flex"
     mainContainer.style.flexDirection = "column"
+    mainContainer.style.justifyContent = "space-evenly"
+    mainContainer.style.alignItems = "center"
+    mainContainer.id = "mainContainer"
      // skapat mainContainer samt lite styling
      
     let titleContainer = document.createElement("div")
-    titleContainer.style.height ="20px"
-    titleContainer.style.innerhtml = title
-
-     // Titeln på telefonen
-
+    titleContainer.id = "titleContainer"
+    titleContainer.style.display.flex = "flex"
+    titleContainer.style.flex = "none"
+    titleContainer.style.fontSize = "50px"
+    titleContainer.style.alignItems = "center"
+    titleContainer.style.justifyContent = "center"
+    titleContainer.style.fontWeight = "700"
+    titleContainer.innerHTML = listOfProducts[3].title
+    // Titeln på telefonen
+    
     let descriptionContainer = document.createElement("div")
-     // Description på telefonen
+    descriptionContainer.style.flex = "none"
+    descriptionContainer.id = "descriptionContainer"
+    descriptionContainer.style.fontSize = "25px"
+    descriptionContainer.style.fontWeight = "700"
+    descriptionContainer.innerHTML = listOfProducts[3].description
+    // Description på telefonen
      
     let imgContainer = document.createElement("img")
-    imgcointainer.style.height= "80%"
-    imgcointainer.style.width= "80%"
-     // bilden
+    imgContainer.style.flex = "none"
+    imgContainer.id = "imgContainer"
+    imgContainer.style.height = "650px"
+    imgContainer.style.width = "374px"
+    imgContainer.src = listOfProducts[3].image
+    
+    // bilden på telefonen
     
     let priceContainer = document.createElement("div")
-     // Priset på telefonen
+    priceContainer.style.flex = "none"
+    priceContainer.id = "priceContainer"
+    priceContainer.innerHTML = listOfProducts[3].price + " " + "kr"
+    priceContainer.style.fontSize = "30px"
+    priceContainer.style.fontWeight = "700"
+    
+    
+    // Priset på telefonen
     
     let buyButton = document.createElement("button")
-     // skicka till varukorg
-    
-    
-    mainContainer.appendChild(descriptionContainer)
+    buyButton.style.flex = "none"
+    buyButton.id = "buyButton"
+    buyButton.style.backgroundColor = "#5596F5"
+    buyButton.innerText = "lägg till i kundvagn"
+    buyButton.style.color = "white"
+    buyButton.style.fontWeight = "700"
+    buyButton.onclick
+
+
+    // skicka till varukorg knappen
+
+   
     mainContainer.appendChild(titleContainer)
+    mainContainer.appendChild(descriptionContainer)
     mainContainer.appendChild(imgContainer)
     mainContainer.appendChild(priceContainer)
     mainContent.appendChild(mainContainer)
     mainContainer.appendChild(buyButton)
+    // appendar divarna till mainContainer
      
     
     
